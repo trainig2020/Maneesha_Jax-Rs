8d6be4c2c7cc3e93cb18e33e3f11ff418b1c4587
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-12 19:36:25 +0530

    Add files via upload

---
+++ JAX-RS_PaginationAndFilteringAndParams/src/main/java/com/ezhil/JAX_RS_PaginationAndFiltering/control/MessageResource.java
@@ -0,0 +1,68 @@
+package com.ezhil.JAX_RS_PaginationAndFiltering.control;
+
+import java.util.List;
+
+import javax.ws.rs.BeanParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.ezhil.JAX_RS_PaginationAndFiltering.control.bean.FilterBean;
+import com.ezhil.JAX_RS_PaginationAndFiltering.model.Message;
+import com.ezhil.JAX_RS_PaginationAndFiltering.service.MessageService;
+
+@Path("/message")
+public class MessageResource {
+	
+	MessageService messageService = new MessageService();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Message> getAllMessage(@BeanParam FilterBean filterBean){
+		if(filterBean.getYear()>0) {
+			return messageService.getMessagesForYear(filterBean.getYear());
+		}
+		if(filterBean.getStart()>=0 && filterBean.getSize()>=0) {
+			return messageService.getMessagePaginated(filterBean.getStart(), filterBean.getSize());
+		}
+		return messageService.getAllMessage();
+	}
+	
+	@GET
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message getMessage(@PathParam("messageId") long messageId) {
+		return messageService.getMessages(messageId);
+	}
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message postMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message putMessage(@PathParam("messageId") long messageId,Message message) {
+		message.setId(messageId);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId") long messageId) {
+		 messageService.removeMessage(messageId);
+	}
+	
+	
+
+}

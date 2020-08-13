dd3a3ed137df867841d31f0c715d42102093acec
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-13 20:56:10 +0530

    Add files via upload

---
+++ Advanced_JaxRs/src/main/java/com/ezhil/resource/MyResource.java
@@ -0,0 +1,20 @@
+package com.ezhil.resource;
+
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("test")
+@Singleton
+public class MyResource {
+	private int  count;
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String test() {
+		count = count+1;
+		return "Test Run where its called "+count+" time(s)";
+	}
+
+}
